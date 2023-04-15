cee479ee232dc9758fd0c45fcab22afa6959591c
demir <huseyin.d3r@gmail.com>
2023-04-15 14:32:56 +0300

    update setuptools

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "insidecouchbase",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "Huseyin Demir",
     author_email = "huseyin.d3r@gmail.com",
     description = "Check and analyze a couchbase cluster in terms of cluster health,bucket,and replication",
