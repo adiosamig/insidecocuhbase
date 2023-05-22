4c134d09cc027c8b524aaec01de647ff4aa9808c
demir <huseyin.d3r@gmail.com>
2023-05-22 08:58:47 +0300

    change memory limit and output variable

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,34 @@
+# Version 2.1.0
+
+## Added
+
+- Table output decleared as a variable
+
+## Changed
+
+- Memory limit ratio changed from 0,7 to 0,8
+- Naming changes on rules.py
+
+## Deprecated
+
+- None
+
+## Removed
+
+- None
+
+## Fixed
+
+- None
+
+## Security 
+
+- None
+
+# Conclusion
+
+This is the changelog for the InsideCouchbase library, which provides features to analyze and monitor a Couchbase cluster. The latest version 1.0.0 includes a new feature to generate health reports for cluster healthchecks and refactored constructors for better consistency and readability. The library is actively maintained, and we encourage you to use the latest version for better performance and functionality.
+
 # Version 1.0.0
 
 ## Added
--- src/couchbase/rules.py
+++ src/couchbase/rules.py
@@ -4,55 +4,64 @@ def setRules():
         "code": "configuration-01",
         "definition" : "Data Memory Quota Configuration",
         "result":"not_checked",
-        "Recommendation" : "Data memory quota can not be greater than %70 of OS memory"
+        "severity": "critical",
+        "recommendation" : "Data memory quota can not be greater than %80 of OS memory"
     },
     {
         "code": "configuration-02",
         "definition" : "SWAP Configuration",
         "result":"not_checked",
-        "Recommendation" : "SWAP can not be used in production couchbase environments"
+        "severity": "critical",
+        "recommendation" : "SWAP can not be used in production couchbase environments"
     },
     {
         "code": "configuration-03",
         "definition" : "All Nodes are Using Same Couchbase Version",
         "result":"not_checked",
-        "Recommendation" : "Different versions of couchbase can not be used inside the same cluster."
+        "severity": "critical",
+        "recommendation" : "Different versions of couchbase can not be used inside the same cluster."
     },
     {
         "code": "configuration-04",
         "definition" : "Cluster is Working Wtih MDS Model",
         "result":"not_checked",
-        "Recommendation" : "1 Couchbase node has to be use 1 service only."
+        "severity": "critical",
+        "recommendation" : "1 Couchbase node has to be use 1 service only."
     },
     {
         "code": "configuration-05",
         "definition" : "Autofailover Enabled",
         "result":"not_checked",
-        "Recommendation" : "Autofailover needs to be enabled to achive better HA"
+        "severity": "critical",
+        "recommendation" : "Autofailover needs to be enabled to achive better HA"
     },
     {
         "code": "health-01",
         "definition" : "All Nodes Up and Running",
         "result":"not_checked",
-        "Recommendation" : "All nodes have to be up and joined cluster."
+        "severity": "critical",
+        "recommendation" : "All nodes have to be up and joined cluster."
     },
     {
         "code": "bucket-01",
         "definition" : "All Buckets Have Resident Ratio Greater Than %50",
         "result":"not_checked",
-        "Recommendation" : "Bucket resident ratio needs to be greater than %50"
+        "severity": "medium",
+        "recommendation" : "Bucket resident ratio needs to be greater than %50"
     },
     {
         "code": "bucket-02",
         "definition" : "All Buckets Have At Least 1 Replica",
         "result":"not_checked",
-        "Recommendation" : "All buckets must have at least one replica to prevent data loss in case of emergency"
+        "severity": "critical",
+        "recommendation" : "All buckets must have at least one replica to prevent data loss in case of emergency"
     },
     {
         "code": "bucket-03",
         "definition" : "All Buckets Have 1024 Primary Vbucket",
         "result":"not_checked",
-        "Recommendation" : "All buckets must carry 1024 primary vbucket."
+        "severity": "critical",
+        "recommendation" : "All buckets must carry 1024 primary vbucket."
     }
 ]
     return ruleList
